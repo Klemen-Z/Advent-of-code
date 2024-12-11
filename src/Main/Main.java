5dce98df13f1d470a372d36acb4c91d3d458f0b8
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2024-12-11 13:38:00 +0100

    day 11 part1/2 buggy code

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,21 @@
 package Main;
 
 import Days.week1.*;
-import Days.week2.*;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
         final long start = System.currentTimeMillis();
-        Day10 day10 = new Day10();
+        Day4 day4 = new Day4();
         Main main = new Main();
         String input = readFile("input.txt");
 
-        day10.solve(input,true);
+        System.out.println("There are " + main.calculateTotalElements(main.arrangementParse(input), 75) + " stones");
 
         final long end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " ms");
@@ -23,6 +23,65 @@ public class Main {
 
     //Workshop space below
 
+    public long calculateTotalElements(ArrayList<Long> numbers, int iterations) {
+        AtomicLong totalElements = new AtomicLong();
+        Map<Long, Long> vals = Collections.synchronizedMap(new HashMap<>());
+
+        numbers.forEach(number -> {
+           blink(number, iterations, vals);
+           System.out.println("Blinked");
+        });
+
+        vals.keySet().parallelStream().forEach(number -> {
+           totalElements.addAndGet(vals.get(number));
+        });
+
+        return totalElements.get();
+    }
+
+    public static boolean hasEvenNumberOfDigits(long number) {
+        int digitCount = String.valueOf(Math.abs(number)).length();
+        return digitCount % 2 == 0;
+    }
+
+    public static Long[] splitNumber(long number) {
+        String numberStr = number+"";;
+
+        long left = Long.parseLong(numberStr.substring(0, numberStr.length()/2));
+        long right = Long.parseLong(numberStr.substring(numberStr.length()/2));
+
+        return new Long[]{left, right};
+    }
+
+    public void blink(Long number, int iterations, Map<Long, Long> map) {
+        if (!map.containsKey(number)) {
+            map.put(number, 1L);
+        } else {
+            map.replace(number, map.get(number) + 1);
+        }
+
+        if (number == 0){
+            blink(1L, iterations-1, map);
+        } else if (hasEvenNumberOfDigits(number)) {
+            Long[] vals = splitNumber(number);
+            for (Long val : vals) {
+                blink(val, iterations-1, map);
+            }
+        } else {
+            long newNum = number*2024;
+            blink(newNum, iterations-1, map);
+        }
+        map.replace(number, map.get(number)-1);
+    }
+
+    public ArrayList<Long> arrangementParse(String input) {
+        ArrayList<Long> arrangement = new ArrayList<>();
+        String[] inputStrings = input.replace("\r\n", "").split(" ");
+        for (String inputString : inputStrings) {
+            arrangement.add(Long.parseLong(inputString));
+        }
+        return arrangement;
+    }
 
     //util functions below
     private static String readFile(String filename) {
