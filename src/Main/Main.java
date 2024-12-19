d2437e153974167149f791d0e184473367e4e81f
Klemen-Z <klemenzmahar@gmail.com>
2024-12-19 20:09:29 +0100

    quick solution, needed help from smart online people.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,19 @@
 package Main;
 
 import Days.week1.*;
+import Days.week2.Day11;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
         final long start = System.currentTimeMillis();
-        Day4 day4 = new Day4();
+        Day11 day11 = new Day11();
         Main main = new Main();
         String input = readFile("input.txt");
 
-        System.out.println("There are " + main.calculateTotalElements(main.arrangementParse(input), 75) + " stones");
+        day11.solve(input, 75);
 
         final long end = System.currentTimeMillis();
         System.out.println("Time taken: " + (end - start) + " ms");
@@ -23,57 +21,7 @@ public class Main {
 
     //Workshop space below
 
-    public long calculateTotalElements(ArrayList<Long> numbers, int iterations) {
-        AtomicLong totalElements = new AtomicLong(numbers.size());
-        Map<Long, Long[]> knownOps = Collections.synchronizedMap(new HashMap<>());
-        knownOps.put(0L, new Long[]{1L});
 
-        numbers.forEach(number -> {
-            final long start = System.currentTimeMillis();
-            blink(number, iterations, totalElements, knownOps);
-            final long end = System.currentTimeMillis();
-            System.out.println("Iteration done, took: " + (end - start) + " ms");
-        });
-
-        return totalElements.get();
-    }
-
-    public void blink(Long number, int iterations, AtomicLong total, Map<Long, Long[]> knownOps) {
-        if (iterations <= 0) {
-            return;
-        }
-
-        if (knownOps.containsKey(number)) {
-            Long[] nums = knownOps.get(number);
-            if (nums.length == 1) {
-               blink(nums[0], iterations-1, total, knownOps);
-               return;
-            }
-
-            total.incrementAndGet();
-            for (Long num : nums) {
-                blink(num, iterations-1, total, knownOps);
-            }
-            return;
-        }
-
-        String numberStr = number+"";
-        if (numberStr.length()%2 == 0) {
-            total.incrementAndGet();
-            long left = Long.parseLong(numberStr.substring(0, numberStr.length()/2));
-            long right = Long.parseLong(numberStr.substring(numberStr.length()/2));
-            Long[] vals = new Long[]{left, right};
-            knownOps.put(number, vals);
-            for (Long val : vals) {
-                blink(val, iterations-1, total, knownOps);
-            }
-            return;
-        }
-
-        long newNum = number*2024;
-        knownOps.put(number, new Long[]{newNum});
-        blink(newNum, iterations-1, total, knownOps);
-    }
 
     public ArrayList<Long> arrangementParse(String input) {
         ArrayList<Long> arrangement = new ArrayList<>();
@@ -82,6 +30,10 @@ public class Main {
             arrangement.add(Long.parseLong(inputString));
         }
         return arrangement;
+    }
+
+    private long getSize(Map<Long, Long> stoneCount) {
+        return stoneCount.values().stream().reduce(0L, Long::sum);
     }
 
     //util functions below
