d14de7929135b8a01f46cc19b4e89a0bc2cf113a
Klemen-Z <klemenzmahar@gmail.com>
2024-12-19 20:03:33 +0100

    Day 1 part 2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,66 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        File f = new File("nums.txt");
+
+        String[] nums = readFile("nums.txt").toString().replace("   ", "\n").replace("\r", "").split("\n");
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = Integer.parseInt(nums[i]);
+            if (i % 2 == 0) {
+                list1.add(num);
+            } else {
+                list2.add(num);
+            }
+        }
+
+        System.out.println(getSimilarity(list1, list2));
+    }
+
+    private static int getSimilarity(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        int finalResult = 0;
+
+        int count = 0;
+
+        for (Integer integer1 : list1) {
+            for (Integer integer2 : list2) {
+                if (integer1.intValue() == integer2.intValue()) {
+                    count++;
+                }
+            }
+            finalResult += integer1*count;
+            count = 0;
+        }
+
+        return finalResult;
+    }
+
+    private static int getTotalDifference(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        int finalResult = 0;
+
+        list2.sort(Integer::compareTo);
+        list1.sort(Integer::compareTo);
+
+        for (int i = 0; i < list1.size(); i++) {
+            int tempVal = 0;
+
+            int int1 = list1.get(i);
+            int int2 = list2.get(i);
+
+            tempVal = int1 - int2;
+            if (tempVal < 0) {
+                tempVal = int2 - int1;
+            }
+            finalResult += tempVal;
+        }
+
+        return finalResult;
+    }
+
+    private static String readFile(String filename) {
+        File f = new File(filename);
         assert f.exists();
 
         FileReader fr;
@@ -33,33 +92,6 @@ public class Main {
             sb.append((char) res);
         }
 
-        String[] nums = sb.toString().replace("   ", "\n").replace("\r", "").split("\n");
-
-        int finalResult = 0;
-
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = Integer.parseInt(nums[i]);
-            if (i % 2 == 0) {
-                list1.add(num);
-            } else {
-                list2.add(num);
-            }
-        }
-        list2.sort(Integer::compareTo);
-        list1.sort(Integer::compareTo);
-
-        for (int i = 0; i < list1.size(); i++) {
-            int tempVal = 0;
-            tempVal = list1.get(i) - list2.get(i);
-            if (tempVal < 0) {
-                tempVal = list2.get(i) - list1.get(i);
-            }
-            finalResult += tempVal;
-        }
-
-        System.out.println(finalResult);
+        return sb.toString();
     }
 }
